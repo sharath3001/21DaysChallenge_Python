e701ca048ea947fdc1e5314e288d116c5fe84634
sharath3001 <52591235+sharath3001@users.noreply.github.com>
2020-03-27 23:33:28 +0530

    Add Code files

---
+++ PracticeCodes/literals.py
@@ -0,0 +1,16 @@
+print("2")
+print(2)
+print(111_234)
+
+print(0o123) # O is also valid
+print(0x231) # X is also valid
+
+print(0.4)
+print(.4)
+print(4.)
+
+print(3e8) # exponent, E is also valid
+
+print("String")
+print('"String"')
+print("\"String\"")
---
+++ PracticeCodes/operators.py
@@ -0,0 +1,20 @@
+print(2+2)
+
+print(2 ** 3) # exponentiation operator
+print(2 ** 3.2) # any one operand is float then result is float
+print(2. ** 3)
+print(2. ** 3.)
+
+print(6 / 3) # division always give float
+
+print(6 // 3) # integer division operator
+print(6 // 3.)
+print(6. // 4)
+print(6. // 3.)
+
+print(12 % 4.5) # remainder or modulo operator
+
+print(16 % 3 % 2) # left binded
+print(2 ** 2 ** 3) # right binded
+
+print(-4  / -3)
