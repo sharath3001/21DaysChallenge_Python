41041446d08a0c0c92e03b3b48dfb04da4ef9602
sharath3001 <52591235+sharath3001@users.noreply.github.com>
2020-04-13 22:38:15 +0530

    Add file
    Added filesText.py

---
+++ PracticeCodes/filesText.py
@@ -0,0 +1,75 @@
+stream = open("text.txt", "rt", encoding = "utf-8") # opening tzop.txt in read mode, returning it as a file object
+print(stream.read()) # printing the content of the file
+from os import strerror
+try:
+    cnt = 0
+    s = open('text.txt', "rt")
+    ch = s.read(1)
+    while ch != '':
+        print(ch, end='')
+        cnt += 1
+        ch = s.read(1)
+    s.close()
+    print("\n\nCharacters in file:", cnt)
+except IOError as e:
+    print("I/O error occurred: ", strerr(e.errno))
+from os import strerror
+try:
+    cnt = 0
+    s = open('text.txt', "rt")
+    content = s.read()
+    for ch in content:
+        print(ch, end='')
+        cnt += 1
+    s.close()
+    print("\n\nCharacters in file:", cnt)
+except IOError as e:
+    print("I/O error occurred: ", strerr(e.errno))
+try:
+    ccnt = lcnt = 0
+    s = open('text.txt', 'rt')
+    line = s.readline()
+    while line != '':
+        lcnt += 1
+        for ch in line:
+            print(ch, end='')
+            ccnt += 1
+        line = s.readline()
+    s.close()
+    print("\n\nCharacters in file:", ccnt)
+    print("Lines in file:     ", lcnt)
+except IOError as e:
+    print("I/O error occurred:", strerr(e.errno))
+try:
+    ccnt = lcnt = 0
+    s = open('text.txt', 'rt')
+    lines = s.readlines(20) # returns list of lines
+    while len(lines) != 0:
+        for line in lines:
+            lcnt += 1
+            for ch in line:
+                print(ch, end='')
+                ccnt += 1
+        lines = s.readlines(10)
+    s.close()
+    print("\n\nCharacters in file:", ccnt)
+    print("Lines in file:     ", lcnt)
+except IOError as e:
+    print("I/O error occurred:", strerr(e.errno))
+from os import strerror
+try:
+	fo = open('newtext.txt', 'wt') # a new file (newtext.txt) is created
+	for i in range(10):
+		s = "line #" + str(i+1) + "\n"
+		for ch in s:
+			fo.write(ch)
+	fo.close()
+except IOError as e:
+	print("I/O error occurred: ", strerr(e.errno))
+try:
+	fo = open('newtext.txt', 'wt')
+	for i in range(10):
+		fo.write("line #" + str(i+1) + "\n")
+	fo.close()
+except IOError as e:
+	print("I/O error occurred: ", strerr(e.errno))
